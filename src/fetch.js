c80d966d6d64249fa13e726663dcb4c65023e602
romancarpangler <korzunroman56@gmail.com>
2023-01-16 15:28:58 +0200

    did home work

--- src/fetch.js
+++ src/fetch.js
@@ -5,8 +5,5 @@ export default async function fetchImages(value, page) {
   const key = '32827257-9a0fc54d12c963eef1dafe00d';
   const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
 
-  return await axios
-    .get(`${url}${filter}`)
-    .then(response => response.data)
-    .catch(err => console.log(err));
+  return await axios.get(`${url}${filter}`).then(response => response.data);
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,104 @@
 import fetchImages from './fetch';
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const input = document.querySelector('.search-form');
-const inputValue = input.value;
-input.addEventListener('submit', onSubmitSearchForm);
+const form = document.querySelector('.search-form');
+form.addEventListener('submit', onSubmitSearchForm);
+const input = document.querySelector('[name="searchQuery"]');
 
 const button = document.querySelector('.load-more');
+button.addEventListener('click', onClickLoadMoreBtn);
 button.classList.add('is-hidden');
 
-const wraper = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
 
-let page = 1;
+let currentPage = 1;
+let currentHits = 0;
 
 async function onSubmitSearchForm(e) {
   e.preventDefault();
 
-  if (inputValue) {
+  inputValue = input.value;
+
+  currentPage = 1;
+
+  if (inputValue === '') {
     return;
   }
 
-  const response = await fetchImages(inputValue, page);
+  const response = await fetchImages(inputValue, currentPage);
+  currentHits += response.hits.length;
 
-  const d = response.totalHits;
-  console.log(d);
+  if (response.totalHits > 40) {
+    button.classList.remove('is-hidden');
+  } else {
+    button.classList.add('is-hidden');
+  }
+
+  try {
+    if (response.totalHits > 0) {
+      Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
+      gallery.innerHTML = '';
+      renderCardImage(response.hits);
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * -100,
+        behavior: 'smooth',
+      });
+    }
+
+    if (response.totalHits === 0) {
+      gallery.innerHTML = '';
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      button.classList.add('is-hidden');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function onClickLoadMoreBtn() {
+  currentPage += 1;
+  const response = await fetchImages(inputValue, currentPage);
+
+  currentHits += response.hits.length;
+
+  if (currentHits === response.totalHits) {
+    button.classList.add('is-hidden');
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+
+  renderCardImage(response.hits);
+}
+
+function renderCardImage(arr) {
+  const markup = arr
+    .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+      return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
